f338d8bfa5d94f07d0724c866b3bb6506531058a
Spyros Kontopantelis <spyros.kontopantelis@gmail.com>
2019-03-15 16:00:09 +0200

    I don't remember the changes

---
+++ nxCarpets/pages/customers/addCustomer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      Contact
+    </h1>
+    <div class="content">
+      <form name="contact" action="" method="post">
+        <label class="form-label" for="name">
+          Name:
+        </label>
+        <input class="form-field" name="name" id="name" />
+        <label class="form-label" for="code">
+          Code:
+        </label>
+        <input class="form-field" name="code" id="code" />
+        <label class="form-label" for="message">
+          Street:
+        </label>
+        <textarea class="form-field" name="street" id="street"></textarea>
+        <input class="form-button" type="submit" value="Add Customer" />
+      </form>
+    </div>
+  </div>
+</template>
